e350482197fba3843eb49898edc6c52bf404bf2c
Lam <lamkyo373@gmail.com>
2024-01-27 20:39:54 +0700

    added feature to add hotel, and e2e test for feature

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,15 @@ import userRoutes from "./routes/users";
 import authRoutes from "./routes/auth";
 import cookieParser from "cookie-parser";
 import path from "path";
+import { v2 as cloudinary } from "cloudinary";
+import myHotelRoutes from "./routes/my-hotels";
+
+//connect to cloudinary
+cloudinary.config({
+	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 //it'll try to connect to our database when we first run the app
@@ -33,6 +42,11 @@ app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 //any requests coming into our api prefixed with /api/users then pass the request to the userRoute
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/my-hotels", myHotelRoutes);
+
+app.get("*", (req: Request, res: Response) => {
+	res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+});
 
 app.listen(7000, () => {
 	console.log("server running on localhost:7000");
