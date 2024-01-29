17d32e3e2aac19a90c9116350c09e852e3c01963
Lam <lamkyo373@gmail.com>
2024-01-29 20:18:29 +0700

    added my hotel views and added my e2e test

--- e2e-tests/tests/manage-hotels.spec.ts
+++ e2e-tests/tests/manage-hotels.spec.ts
@@ -3,53 +3,6 @@ import path from "path";
 
 const UI_URL = "http://localhost:5173/";
 
-// test.beforeEach(async ({ page }) => {
-// 	await page.goto(UI_URL);
-
-// 	//get the sign in button
-// 	await page.getByRole("link", { name: "Sign In" }).click();
-
-// 	//check if the signin text or heading appear in the browser\
-// 	await expect(page.getByRole("heading", { name: "Sign In" })).toBeVisible();
-
-// 	await page.locator("[name=email]").fill("4.1@3.com");
-// 	await page.locator("[name=password]").fill("1234567");
-
-// 	await page.getByRole("button", { name: "Login" }).click();
-
-// 	//add an assertion to tell that the user has actually signed in sucessfully
-// 	await expect(page.getByText("Sign in Sucessful!")).toBeVisible();
-// });
-
-// test("should allow user to add a hotel", async ({ page }) => {
-// 	await page.goto(`${UI_URL}add-hotel`);
-
-// 	await page.locator('[name="name"]').fill("Test Hotel");
-// 	await page.locator('[name="city"]').fill("Test City");
-// 	await page.locator('[name="country"]').fill("Test Country");
-// 	await page
-// 		.locator('[name="description"]')
-// 		.fill("This is the description for the Test Hotel");
-// 	await page.locator('[name="pricePerNight"]').fill("100");
-// 	await page.selectOption('[select[name="starRating"]', "3");
-
-// 	await page.getByText("Budget").click();
-
-// 	await page.getByLabel("Free Wifi").check();
-// 	await page.getByLabel("Parking").check();
-
-// 	await page.locator('[name="adultCount"]').fill("2");
-// 	await page.locator('[name="childCount"]').fill("4");
-
-// 	await page.setInputFiles('[name="imageFiles"]', [
-// 		path.join(__dirname, "files", "1.png"),
-// 		path.join(__dirname, "files", "2.png"),
-// 	]);
-
-// 	await page.getByRole("button", { name: "Save" }).click();
-// 	await expect(page.getByText("Hotel Saved!")).toBeVisible();
-// });
-
 test.beforeEach(async ({ page }) => {
 	await page.goto(UI_URL);
 
@@ -58,12 +11,14 @@ test.beforeEach(async ({ page }) => {
 
 	await expect(page.getByRole("heading", { name: "Sign In" })).toBeVisible();
 
-	await page.locator("[name=email]").fill("4.1@3.com");
-	await page.locator("[name=password]").fill("1234567");
+	await page.locator("[name=email]").fill("1@1.com");
+	await page.locator("[name=password]").fill("password123");
 
 	await page.getByRole("button", { name: "Login" }).click();
 
-	await expect(page.getByText("Sign in Successful!")).toBeVisible();
+	await expect(page.getByText("Sign in Successful!")).toBeVisible({
+		timeout: 5000,
+	});
 });
 
 test("should allow user to add a hotel", async ({ page }) => {
@@ -92,6 +47,22 @@ test("should allow user to add a hotel", async ({ page }) => {
 	]);
 
 	await page.getByRole("button", { name: "Save" }).click();
-
 	await expect(page.getByText("Hotel Saved!")).toBeVisible();
+});
+
+test("should display hotels", async ({ page }) => {
+	await page.goto(`${UI_URL}my-hotels`);
+
+	await expect(page.getByText("Dublin Getaways")).toBeVisible();
+	await expect(page.getByText("Lorem ipsum dolor sit amet")).toBeVisible();
+	await expect(page.getByText("Dublin, Ireland")).toBeVisible();
+	await expect(page.getByText("All Inclusive")).toBeVisible();
+	await expect(page.getByText("£119 per night")).toBeVisible();
+	await expect(page.getByText("2 adults, 3 children")).toBeVisible();
+	await expect(page.getByText("2 Star Rating")).toBeVisible();
+
+	await expect(
+		page.getByRole("link", { name: "View Details" }).first()
+	).toBeVisible();
+	await expect(page.getByRole("link", { name: "Add Hotel" })).toBeVisible();
 });
